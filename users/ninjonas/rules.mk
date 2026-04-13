281913ae2d5f309b01b257c834906a4ad6d86b17
ninjonas <jonas@shelley.app>
2026-04-12 23:52:03 -0700

    fix: disable MOUSEKEY_ENABLE to fit within ATmega32U4 flash limit

--- users/ninjonas/rules.mk
+++ users/ninjonas/rules.mk
@@ -1,5 +1,5 @@
 BOOTMAGIC_ENABLE = no       # Disable Boot Magic (https://beta.docs.qmk.fm/features/feature_bootmagic)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys — disabled to fit flash size
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 TAP_DANCE_ENABLE = yes      # Enable Tap Dance.
 COMBO_ENABLE = yes          # Enable Combos
